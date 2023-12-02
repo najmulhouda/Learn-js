db1a0ad4e0672fd78b6d73b0c5c9b508d2573554
Najmul Houda <74393619+najmulhouda@users.noreply.github.com>
2023-12-02 16:57:04 +0000

    modern js

---
+++ ecmaScript23.js
@@ -0,0 +1,10 @@
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const reverseArr = arr.reverse();
+const reverseArray = arr.toReversed();
+// console.log(reverseArray);
+// console.log(arr);
+
+
+const spliceArr = arr.splice(-2 , -2, 100, 400);
+console.log(spliceArr);
+console.log(arr);        
