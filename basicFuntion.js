f6baeb39d32ec7b92101e1a67d31701105290eaa
Najmul Houda <74393619+najmulhouda@users.noreply.github.com>
2023-12-02 15:03:55 +0000

    funtion basic and schope

--- basicFuntion.js
+++ basicFuntion.js
@@ -6,7 +6,7 @@ function sayMyName(name) {
     console.log(name);
 }
 
-sayMyName('Luiz');
+// sayMyName('Luiz');
 
 
 function addTwoNumbers(num1, num2) {
@@ -14,7 +14,7 @@ function addTwoNumbers(num1, num2) {
 }
 
 const result = addTwoNumbers(1, 2);
-console.log(result);
+// console.log(result);
 
 function loginUserMesssage(name = 'Guest') {
     return `Welcome ${name}`;
@@ -22,4 +22,31 @@ function loginUserMesssage(name = 'Guest') {
 
 loginUserMesssage();
 loginUserMesssage('John');
-console.log(loginUserMesssage("Najmul"));
+// console.log(loginUserMesssage("Najmul"));
+
+
+function calculateCartPrice(...num1){
+return num1
+}
+
+// console.log(calculateCartPrice(100 , 400, 500, 600, 700, 800, 900, 1000));
+
+
+const user = {
+    name: 'John',
+    age: 30
+}
+
+function handleObject(anyObject) {
+    console.log(`Username is ${anyObject.name} and age is ${anyObject.age}`);
+}
+handleObject(user);
+
+
+const myNewArray = [1, 2, 3, 4, 5];
+
+function returnSecondItem(myArray) {
+    return myArray[1];
+}
+
+console.log(returnSecondItem(myNewArray));
---
+++ scopes.js
@@ -0,0 +1,18 @@
+// let a = 10
+// const b= 20
+// var c = 30
+// console.log(a, b, c)
+
+// {} carlebraces mostly called scopes when it comes to js
+
+
+if (true) {
+    let a = 100
+    const b = 200
+    var c = 300
+    // console.log(a, b, c)
+}
+
+// console.log( a)
+// console.log( b)
+console.log( c)
