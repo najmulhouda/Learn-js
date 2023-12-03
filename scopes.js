4631eece78fd69b2125063611ec41c70c5090b3f
Najmul Houda <74393619+najmulhouda@users.noreply.github.com>
2023-12-03 14:30:36 +0000

    scopes

--- scopes.js
+++ scopes.js
@@ -1,5 +1,5 @@
-// let a = 10
-// const b= 20
+let a = 10
+const b= 20
 // var c = 30
 // console.log(a, b, c)
 
@@ -7,12 +7,25 @@
 
 
 if (true) {
-    let a = 100
-    const b = 200
+    // let a = 100
+    // const b = 200
     var c = 300
     // console.log(a, b, c)
 }
 
 // console.log( a)
 // console.log( b)
-console.log( c)
+// console.log( c)
+
+function one(){
+    const name = 'najmul'
+    function two(){
+        // console.log(name)
+        const newName = 'najmul houda'
+        //  console.log(newName)
+    }
+   
+    two()
+}
+
+one()
