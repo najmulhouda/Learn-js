2c87d0ccd89075c4aefef347284c8205b2d4c000
Najmul Houda <74393619+najmulhouda@users.noreply.github.com>
2023-12-27 07:44:12 +0000

    event

--- Project/Clock/index.js
+++ Project/Clock/index.js
@@ -6,4 +6,5 @@ setInterval(() => {
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
     clock.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`
+    
 })
